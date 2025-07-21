f866f13969ecc98177cb775b99faebb652086afd
mr-pairot <mr.pairot@gmail.com>
2025-07-21 11:09:24 +0700

    Update setLegend.js

--- js/setLegend.js
+++ js/setLegend.js
@@ -1,21 +1,20 @@
+// setLegend.js
 function addLegend(map, features) {
+  const updateDate = features?.[0]?.properties?.update_date || "ไม่ระบุวันที่";
+
   const legend = L.control({ position: "bottomleft" });
 
   legend.onAdd = function () {
     const div = L.DomUtil.create("div", "info legend");
-
-    const date = features?.[0]?.properties?.update_date || "<ไม่ระบุวันที่>";
-
-    div.innerHTML = `
-      <strong>ข้อมูลพื้นที่เวนคืน วันที่ ${date}</strong><br>
+    div.innerHTML = `<strong>ข้อมูลพื้นที่เวนคืน วันที่ ${updateDate}</strong><br>
       <i style="background: gray; border:1px dashed gray;"></i> ไม่มีข้อมูล<br>
       <i style="background: red;"></i> เข้าพื้นที่ไม่ได้<br>
       <i style="background: green;"></i> เข้าพื้นที่ได้<br>
       <i style="background: yellow;"></i> เข้าพื้นที่ไม่ได้บางส่วน
     `;
-
     return div;
   };
 
   legend.addTo(map);
+  return legend;
 }
