aea04f3f197fb7c97cf35e25ab80820cd515c03f
mr-pairot <mr.pairot@gmail.com>
2025-07-21 10:16:24 +0700

    Update setLegend.js

--- js/setLegend.js
+++ js/setLegend.js
@@ -1,16 +1,39 @@
 // setLegend.js
-const legend = L.control({ position: "bottomleft" });
 
-legend.onAdd = function (map) {
-  const div = L.DomUtil.create("div", "info legend");
-  div.innerHTML = `
-    <strong>ข้อมูลพื้นที่เวนคืน วันที่ 01/07/2568</strong><br>
-    <i style="background: gray; border:1px dashed gray;"></i> ไม่มีข้อมูล<br>
-    <i style="background: red;"></i> เข้าพื้นที่ไม่ได้<br>
-    <i style="background: green;"></i> เข้าพื้นที่ได้<br>
-    <i style="background: yellow;"></i> เข้าพื้นที่ไม่ได้บางส่วน
-  `;
-  return div;
-};
+function addLegend(map) {
+  const legend = L.control({ position: "bottomleft" });
 
-legend.addTo(map);
+  legend.onAdd = function () {
+    const div = L.DomUtil.create("div", "info legend");
+
+    let html = `<strong>ข้อมูลพื้นที่เวนคืน วันที่ 01/07/2568</strong><br>`;
+    
+    // ตัวอย่างข้อมูลจำลอง (mock) เพื่อดึง style
+    const items = [
+      { label: "ไม่มีข้อมูล", value: "#N/A" },
+      { label: "เข้าพื้นที่ไม่ได้", value: 0 },
+      { label: "เข้าพื้นที่ได้", value: 1 },
+      { label: "เข้าพื้นที่ไม่ได้บางส่วน", value: 2 }
+    ];
+
+    
+
+    items.forEach((item) => {
+      const mockFeature = {
+        properties: {
+          "Landpay14-": item.value
+        }
+      };
+      const style = getPolygonStyle(mockFeature);
+
+      const border = style.dashArray ? "1px dashed" : "1px solid";
+
+      html += `<i style="background:${style.fillColor}; border:${border} ${style.color};"></i> ${item.label}<br>`;
+    });
+
+    div.innerHTML = html;
+    return div;
+  };
+
+  legend.addTo(map);
+}
